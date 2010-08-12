b600a96e1f024f5b9cf35901e80cfaeef3bb4e60
Tom Benner <tbb4n@virginia.edu>
2010-08-12 18:50:43 -0400

    Updating mms_engine

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,8 @@
-/*=========== Side-Column ==========*/
 jQuery(document).ready(function(){
-  $('#fxSideMenu').hide();
+  /*=========== Side-Column ==========*/
+  jQuery('#fxSideMenu').hide();
   jQuery("#sideMenuLink").css("background-position", "0% 0%");
+  
+  // When media popups are close, we need to reset the universal tabs' URLs to be not entity-specific
+  jQuery('.thl-popup-close').live('click', function(){ universal_navigation.resetTabs(); });
 });
