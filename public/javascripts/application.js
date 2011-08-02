49aadd4a4fe9be3882f6466497d0495409cbefe7
Andres Montano <amontano@virginia.edu>
2011-08-01 20:48:52 -0600

    updating engines

--- public/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-  /*=========== Side-Column ==========*/
-  ToggleSideMenu();
-  /* jQuery('#fxSideMenu').hide();
-  jQuery("#sideMenuLink").css("background-position", "0% 0%"); */
-  
-  // When media popups are close, we need to reset the universal tabs' URLs to be not entity-specific
-  //jQuery('.thl-popup-close').live('click', function(){ universal_navigation.resetTabs(); });
-});
