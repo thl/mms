96af257f3fff7aae930304464780b84ecfb0fe52
Andres Montano <amontano@virginia.edu>
2011-08-01 18:20:10 -0600

    updating js for hiding side menu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function(){
   /*=========== Side-Column ==========*/
-  jQuery('#fxSideMenu').hide();
-  jQuery("#sideMenuLink").css("background-position", "0% 0%");
+  ToggleSideMenu();
+  /* jQuery('#fxSideMenu').hide();
+  jQuery("#sideMenuLink").css("background-position", "0% 0%"); */
   
   // When media popups are close, we need to reset the universal tabs' URLs to be not entity-specific
   //jQuery('.thl-popup-close').live('click', function(){ universal_navigation.resetTabs(); });
