554202ff2cf2a36ad217a5ee3f1fd32c6e93347c
Andres Montano <amontano@virginia.edu>
2012-07-02 12:59:42 -0600

    updating engines

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
-Mms::Application.config.session_store :cookie_store, :key => ThlCookie.session_key, :domain => '.thlib.org'
+Mms::Application.config.session_store :cookie_store, :key => ThlCookie.session_key #, :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
