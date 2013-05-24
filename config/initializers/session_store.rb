fdb86eecc00c009cdb847c32ad04a8e5e12324d7
Andres Montano <amontano@virginia.edu>
2013-05-24 01:07:59 -0600

    updating engines

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ Mms::Application.config.session_store :cookie_store, :key => ThlCookie.session_k
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Kmaps::Application.config.session_store :active_record_store
+# Mms::Application.config.session_store :active_record_store
